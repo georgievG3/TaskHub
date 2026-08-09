71fb34e618b7de35e2d1e18520989824bae6886b
Georgi Georgiev <156510257+georgievG3@users.noreply.github.com>
2026-08-09 15:09:02 +0300

    Added login func and me auth

--- backend/src/apps/users/views.py
+++ backend/src/apps/users/views.py
@@ -1,5 +1,9 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 from .serializers import RegisterSerializer
 
@@ -7,3 +11,14 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "id": request.user.id,
+            "email": request.user.email,
+            "first_name": request.user.first_name,
+            "last_name": request.user.last_name,
+        })
